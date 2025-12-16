9194a4a8c3be725f221cf8b99d0d463eea7a3c1d
omar-shandaq <omar.shandaq@hotmail.com>
2025-12-16 10:45:17 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -9,7 +9,7 @@ export const LAST_RECOMMENDATIONS_KEY = "skillMatchLastRecommendations";
 
 // Proxy URL
 export const GEMINI_PROXY_URL = 
-  "https://backend-vercel-repo-git-main-jouds-projects-8f56041e.vercel.app/api/gemini-proxy";
+  "https://ai-agent-cv-v3-15-12-6pn2-git-main-omar-shandaqs-projects.vercel.app/api/chat";
 
 import { loadCertificates, getCertificatesDatabase } from "./certificates-data.js";
 //Ghaith's change start
